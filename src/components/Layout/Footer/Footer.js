5b763ea60afc9316f86c16eee7c6cb423f30c141
Killian <killian@studocu.com>
2020-03-14 18:18:20 +0100

    Translate "buy me a coffee"

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Footer.module.scss";
 import classNames from "classnames";
+import { FormattedMessage } from "react-intl";
 
 function Footer({ className }) {
     return (
@@ -16,12 +17,12 @@ function Footer({ className }) {
                         src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
                         alt="Buy me a coffee"
                     />
-                    <span className={styles.text}>Buy me a coffee</span>
+                    <span className={styles.text}>
+                        <FormattedMessage id="buy_me_a_coffee" />
+                    </span>
                 </a>
             </div>
-            <div>
-                © Killian Saint cricq, 2018-2020
-            </div>
+            <div>© Killian Saint cricq, 2018-2020</div>
         </footer>
     );
 }
